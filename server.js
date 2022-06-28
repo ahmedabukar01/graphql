f14b18a39eed1d2cdf890f275f5af5485a2b3b07
ahmed <ahmedlearn01gmail.com>
2022-06-28 16:33:16 +0300

    frustrated

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql')
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}))
 
 app.listen(5000, ()=>{
     console.log('server is running ...')
